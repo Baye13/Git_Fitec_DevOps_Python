4277d9e4a8756100f0246e09fc05ac5e328711be
mcam <mcam@M4FR2089.fr.meta4.com>
2020-08-12 17:01:16 +0200

    Commit 3

--- src/FileToSynchroniseWithGitRepo.java
+++ src/FileToSynchroniseWithGitRepo.java
@@ -2,4 +2,13 @@
 public class FileToSynchroniseWithGitRepo {
 	//Add new filed
 	public int _newfiels;
+	
+	//Seond commit
+	public int _secondField;
+	
+	//Third commit
+	public int _thirdField;
+	
+	//Four commit
+	public int _fourField;
 }
